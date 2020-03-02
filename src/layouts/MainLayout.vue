34a3176415ed9b43322d01072d505ecf60959dc1
Maksym Medved <maxmed91@gmail.com>
2020-03-02 07:45:04 +0200

    commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="app-main-layout">
+    
+  <navbar @click="isOpen = !isOpen" />
+
+  <sidebar v-model="isOpen" />
+  
+    <main class="app-content" :class="{full : !isOpen}">
+      <div class="app-page">
+        <router-view />
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  import navbar from '@/components/app/Navbar'
+  import sidebar from '@/components/app/Sidebar'
+
+  export default {
+    data () {
+      return {
+        isOpen: true
+      }
+    },
+    name: 'main-layout',
+    components: {
+      navbar,
+      sidebar
+    }
+  } 
+</script>
